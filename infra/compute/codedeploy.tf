a5ba76e1531cfaab72b78f9558188a0c64d89c79
maxsanchezmds <maximilianosanchez1304@gmail.com>
2026-04-24 14:40:19 -0400

    [fix] eliminando tiempo de gracia de codedeploy

--- infra/compute/codedeploy.tf
+++ infra/compute/codedeploy.tf
@@ -52,7 +52,7 @@ resource "aws_codedeploy_deployment_group" "kong" {
   blue_green_deployment_config {
     terminate_blue_instances_on_deployment_success {
       action                           = "TERMINATE"
-      termination_wait_time_in_minutes = 5
+      termination_wait_time_in_minutes = 0
     }
 
     deployment_ready_option {
